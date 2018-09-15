1a6a23319327648ad02518ff8ec79fe89a54680a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-09-15 16:01:22 -0400

    Initial Commit

---
+++ Assets/_Scripts/UIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+	public Text helloLabel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clickMeButton() {
+		Debug.Log("Clicked!");
+		helloLabel.text = "Hello, World!";
+	} 
+}
